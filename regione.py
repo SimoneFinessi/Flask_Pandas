880f4bc33e747e762a83ff389a21a7d07609d42a
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-04-04 09:44:21 +0000

    creato comando installazione libbrerie

--- regione.py
+++ regione.py
@@ -1,22 +1,15 @@
 from flask import Flask, render_template, request
-import geopandas as pd
-"app = Flask(__name__)"
-comuni = pd.read_file("{{ url_for('Limiti01012023_g', filename='Com01012023_g/Com01012023_g_WGS84.shp') }}")
-print(comuni)
-"""@app.route('/')
+import geopandas as gpd
+app = Flask(__name__)
+comuni = gpd.read_file("/workspace/Flask_Pandas/Limiti01012023_g/Com01012023_g/Com01012023_g_WGS84.shp")
+prov = gpd.read_file("/workspace/Flask_Pandas/Limiti01012023_g/ProvCM01012023_g/ProvCM01012023_g_WGS84.shp")
+reg = gpd.read_file("/workspace/Flask_Pandas/Limiti01012023_g/Reg01012023_g/Reg01012023_g_WGS84.shp")
+@app.route('/')
 def home():
-    comuni = dati_regioni.drop_duplicates(subset=['denominazione_regione'])
-    return render_template('html.html', list= list(nomi_regioni['denominazione_regione']))
+    
+    return render_template('regioni.html', list=comuni[comuni["COMUNE"]=="Milano"].to_html())
 
-@app.route('/search', methods = ['GET'])
-def search():
-    regione = request.args['regione']
-    risultato = dati_regioni[dati_regioni['denominazione_regione']==regione.capitalize()]
-    if len(risultato) == 0:
-        table = 'Regione non trovata'
-    else:
-        table = risultato.to_html()
-    return render_template('table.html', tabella = table)
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)"""
+  app.run(host='0.0.0.0', port=3245, debug=True)
